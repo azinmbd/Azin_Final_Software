6ee5725577091c1df4a93c4bfc08a522b8a6a1c6
Azin <48130819+azinmbd@users.noreply.github.com>
2023-08-11 18:24:23 -0700

    prompt

--- src/main/java/com/example/demo/controller/SalesController.java
+++ src/main/java/com/example/demo/controller/SalesController.java
@@ -12,34 +12,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 import java.util.List;
+
 @Controller // Add this annotation to indicate that this is a controller component
 @RequestMapping("/sales")
 public class SalesController {
 
-        private final SalesRepository salesRepository;
+    private final SalesRepository salesRepository;
 
-        @Autowired
-        public SalesController(SalesRepository salesRepository) {
-            this.salesRepository = salesRepository;
-        }
-
-        @GetMapping("/list")
-        public String listSales(Model model) {
-            List<Sales> salesList = salesRepository.findAll();
-            model.addAttribute("salesList", salesList);
-            return "sales/list"; // Assuming you have a Thymeleaf template named "list.html" in a "sales" folder
-        }
-
-        @GetMapping("/add")
-        public String showAddForm(Model model) {
-            Sales sales = new Sales();
-            model.addAttribute("sales", sales);
-            return "sales/add"; // Assuming you have a Thymeleaf template named "add.html" in a "sales" folder
-        }
-
-        @PostMapping("/add")
-        public String addSales(@ModelAttribute("sales") Sales sales) {
-            salesRepository.save(sales);
-            return "redirect:/sales/list";
-        }
+    @Autowired
+    public SalesController(SalesRepository salesRepository) {
+        this.salesRepository = salesRepository;
     }
+
+    @GetMapping("/list")
+    public String listSales(Model model) {
+        List<Sales> salesList = salesRepository.findAll();
+        model.addAttribute("salesList", salesList);
+        return "sales/list"; // Assuming you have a Thymeleaf template named "list.html" in a "sales" folder
+    }
+
+    @PostMapping("/add")
+    public String addSales(@ModelAttribute("sales") Sales sales) {
+        if (sales.getQty() == 0) {
+            // Show an alert if the quantity is 0
+            return "redirect:/sales/add?error=quantity";
+        }
+        salesRepository.save(sales);
+        return "redirect:/sales/list";
+    }
+
+
+}
